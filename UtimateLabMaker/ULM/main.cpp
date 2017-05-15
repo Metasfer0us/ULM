3a06da79f848fc1a0b2203889a2a1bfe8e18c6d1
Metasfer0us <metasfer0us@outlook.com>
2017-05-15 18:58:03 +0300

    base

--- UtimateLabMaker/ULM/main.cpp
+++ UtimateLabMaker/ULM/main.cpp
@@ -1,10 +1,4 @@
 #include "common.h"
 
-void main() {
-	bool toKek = true;
-	while (toKek) {
-		char* kek = "bovkush_pidar";
-		std::cout << kek << "\n\n" << "Kek ? : ";
-		std::cin >> toKek;
-	}
+void main() {	
 }
